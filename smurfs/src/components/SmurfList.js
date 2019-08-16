8ac8a74198d6ec45fba4c3b59dd9526afd59d9ae
SierraOG <sierrao@utexas.edu>
2019-08-16 16:56:10 -0500

    added loader

--- smurfs/src/components/SmurfList.js
+++ smurfs/src/components/SmurfList.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import { connect } from 'react-redux';
 
+import Loader from 'react-loader-spinner';
 import Smurf from './Smurf'
 
 const SmurfList = props => {
     return (
         <div className='smurf-list'>
             <h2>Smurf village</h2>
-            {props.smurfs.map(smurf => <Smurf key={smurf.name} smurf={smurf} />)}
+            {props.isLoading ? (
+            <Loader type="TailSpin" color="#686868" height="100" width="100" />
+            ) : (
+            props.smurfs.map(smurf => <Smurf key={smurf.name} smurf={smurf} />)
+            )}
         </div>
     )
 }
